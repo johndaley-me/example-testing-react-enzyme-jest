88c480d10a831546b256e0b7d253a3a8fade992f
John Daley <johndaley.me@gmail.com>
2017-05-09 22:02:06 -0400

    Add test to spy button click

--- src/Car.js
+++ src/Car.js
@@ -5,14 +5,16 @@ import Engine from './Engine';
 // testing hint: export the named render function for unit testing
 // as the default exported component at the end may be a Higher Order Component
 // with external dependencies not relevant to the unit test
-export const Car = ({ color }) => (
+export const Car = ({ color, handleStartCar }) => (
   <div>
     {/* Find and test dynamic data by id */}
     <h1 id='car--heading'>I am a {color} car</h1>
     <Engine type='diesel' />
+    <button id='car--start' type='button' onClick={handleStartCar}>Start</button>
   </div>
 );
 Car.propTypes = {
   color: PropTypes.string.isRequired,
+  handleStartCar: PropTypes.func.isRequired
 };
 export default Car;
--- src/Car.test.js
+++ src/Car.test.js
@@ -4,25 +4,32 @@ import { Car } from './Car';
 import Engine from './Engine';
 
 describe('<Car />', () => {
-  describe('renders', () => {
-    let wrapper;
-    beforeAll(() => {
-      wrapper = shallow(<Car color='red' />);
-    });
-    // check dynamic data renders correctly
-    // find elements by ID
-    it('renders the heading', () => {
-      const heading = wrapper.find('#car--heading');
-      expect(heading).toHaveLength(1);
-      expect(heading.text()).toEqual('I am a red car');
-    });
-    // check child components are called correctly
-    it('has a diesel engine', () => {
-      // testing hint: If the child component is a composed component
-      // then you will likely need to find by class otherwise
-      // finding by string (e.g. 'Engine') will work
-      const engine = wrapper.find(Engine);
-      expect(engine.prop('type')).toEqual('diesel');
-    });
+  let wrapper;
+  let handleStartCar;
+  beforeAll(() => {
+    handleStartCar = jest.fn();
+    wrapper = shallow(<Car color='red' handleStartCar={handleStartCar} />);
+  });
+  // check dynamic data renders correctly
+  // find elements by ID
+  it('renders the heading', () => {
+    const heading = wrapper.find('#car--heading');
+    expect(heading).toHaveLength(1);
+    expect(heading.text()).toEqual('I am a red car');
+  });
+  // check child components are called correctly
+  it('has a diesel engine', () => {
+    // testing hint: If the child component is a composed component
+    // then you will likely need to find by class otherwise
+    // finding by string (e.g. 'Engine') will work
+    const engine = wrapper.find(Engine);
+    expect(engine.prop('type')).toEqual('diesel');
+  });
+  // check actions
+  it('calls hanldeStartCar on button click', function () {
+    const button = wrapper.find('#car--start');
+    expect(button).toHaveLength(1);
+    button.simulate('click');
+    expect(handleStartCar).toHaveBeenCalledTimes(1);
   });
 });
